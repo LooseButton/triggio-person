370fb812092c776eefc092bdaf927baddf17f609
Draco Li <draco@dracoli.com>
2013-07-18 02:09:36 -0400

    added delay

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -10,7 +9,8 @@ var express = require('express')
   , path = require('path')
   , sys = require('sys')
   , exec = require('child_process').exec
-  , underscore = require('underscore')
+  , __ = require('underscore')
+  , rest = require('restler')
   , yaml = require('js-yaml')
   , validDevices = require(process.cwd() + "/public/assets/devices.yml");
 
@@ -39,8 +39,31 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+
+// Store our initial connections
+var initialConnections = [],
+    connectedDevices = [],
+    TRIGGIO_AUTH_TOKEN = 'NzM3ZTcxODQ5MWRiMTYwNTA4OGRjMTY2',
+    MINIMAL_TIME_DIFFERENCE = 10 * 60 * 1000;
+
 var sendToTriggio = function(person, msg, sound) {
   console.log("Send to Triggio: " + person + " - " + msg + " - " + sound);
+  var data = {
+    'source': person,
+    'sound_id': sound,
+    'type': person,
+    'message': msg
+  };
+  rest.post('http://triggio.meteor.com/api/events', {
+    data: JSON.stringify(data),
+    headers: {
+      'Accept': '*/*',
+      'content-type': 'text/plain',
+      'X-Auth-Token': TRIGGIO_AUTH_TOKEN
+    }
+  }).on('complete', function(data, response) {
+    console.log('Triggio Msg Successfully Sent!');
+  });
 };
 
 var parseConnections = function(callback) {
@@ -71,44 +94,66 @@ var parseConnections = function(callback) {
   return true;
 };
 
-// Store our initial connections
-var initialConnections = [];
-
-var connectedDevices = [];
-
 var addConnectedDevice = function(device) {
-  connectedDevices.push(device);
+  // Add this new device only if it is connect longer than minimal time
+  now = new Date();
+  deviceFound = false;
+  for (var i = connectedDevices.length - 1; i >= 0; i--) {
+    targetDevice = connectedDevices[i];
+    if (targetDevice['address'] === device['address']) {
+      deviceFound = true;
+      if (now - targetDevice['connected_time'] >= MINIMAL_TIME_DIFFERENCE) {
+        targetDevice['connected_time'] = now;
+        return true;
+      }
+    }
+  }
+
+  // Add this device if it was never connected
+  if (!deviceFound) {
+    device['connected_time'] = now;
+    connectedDevices.push(device);
+    return true;
+  }
+
+  // Device found but didn't pass minimal connected time
+  return false;
 };
 
 // Listen for new connections every 10 sec
 var checkForNewConnections = function() {
   parseConnections(function(newConnections) {
-    var newMacs = underscore.pluck(newConnections, 'mac');
-    var oldMacs = underscore.pluck(initialConnections, 'mac');
+    var newMacs = __.pluck(newConnections, 'mac');
+    var oldMacs = __.pluck(initialConnections, 'mac');
 
     console.log('new macs: \n' + newMacs);
     console.log('old macs: \n' + oldMacs);
 
     // Get all new connections
-    var diffConnections = underscore.difference(newMacs, oldMacs);
+    var diffConnections = __.difference(newMacs, oldMacs);
 
     if (diffConnections.length === 0) {
       console.log('No new devices found!');
+    }else {
+      console.log('new devices found!');
+      console.log("Diff connections:");
+      console.log(diffConnections);
+
+      // Send Triggio events to new mac addresses thats in our list
+      validDevices.forEach(function(device) {
+        if (__.contains(diffConnections, device['address'])) {
+
+          // Try to add connected device, if successfully send triggio event
+          if (addConnectedDevice(device)) {
+            sendToTriggio(device['name'],
+                        device['msg'],
+                        device['soundName']);
+          }
+        }
+      });
     }
 
-    // Send Triggio events to new mac addresses thats in our list
-    validDevices.forEach(function(device) {
-      if (underscore.contains(diffConnections, device['mac'])) {
-        // Send notification to triggio that new person has arrived
-        sendToTriggio(device['name'],
-                      device['msg'],
-                      device['soundName']);
-
-        addConnectedDevice(device);
-      }
-    });
-
-    initialConnections = newConnections;
+    initialConnections = __.clone(newConnections);
 
     // Check again in 8 sec
     setTimeout(function() {
@@ -122,8 +167,11 @@ parseConnections(function(connections) {
   initialConnections = connections;
 
   // Populate already connected devices
+  initialMacs = __.pluck(initialConnections, 'mac');
+  now = new Date();
   validDevices.forEach(function(device) {
-    if (underscore.contains(initialConnections, device['mac'])) {
+    if (__.contains(initialMacs, device['address'])) {
+      device['created_time'] = now;
       connectedDevices.push(device);
     }
   });
